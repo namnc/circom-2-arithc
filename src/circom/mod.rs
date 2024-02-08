30c87613c66ee193c692995cd9aa79ced601a443
Brechy <brechy.web3@gmail.com>
2024-02-08 19:23:46 -0300

    fix: clippy

--- src/circom/mod.rs
+++ src/circom/mod.rs
@@ -3,6 +3,8 @@
 //! This module is a slighly modified version of the original code from the <https://github.com/iden3/circom/> repository `circom` sub module, related to
 //! the circom program structure, project configuration and compilation.
 
+#![allow(clippy::result_unit_err)]
+
 pub mod compilation;
 pub mod execution;
 pub mod input;
