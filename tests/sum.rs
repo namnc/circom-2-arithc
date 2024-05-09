ae52138df7a8e37e6479dba1a2d47780dea6fc37
Andrew Morris <voltrevo@gmail.com>
2024-05-09 10:50:20 +1000

    Fix warnings

--- tests/sum.rs
+++ tests/sum.rs
@@ -6,7 +6,7 @@ const TEST_FILE_PATH: &str = "./tests/circuits/sum.circom";
 fn test_sum() {
     let input = Input::new(TEST_FILE_PATH.into(), "./".into()).unwrap();
     let circuit = build_circuit(&input).unwrap();
-    let mut sim_circuit = circuit.build_sim_circuit().unwrap();
+    let sim_circuit = circuit.build_sim_circuit().unwrap();
 
     let circuit_input = vec![1, 2];
     let res = sim_circuit.execute(&circuit_input).unwrap();
