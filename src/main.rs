809ce17a8f18c1e466e8b1c928e97847ee95105d
Andrew Morris <voltrevo@gmail.com>
2024-06-12 10:33:40 +1000

    Use to_string_pretty for report.json

--- src/main.rs
+++ src/main.rs
@@ -12,7 +12,7 @@ use circom_2_arithc::{
 use clap::Parser;
 use dotenv::dotenv;
 use env_logger::{init_from_env, Env};
-use serde_json::{to_string, to_string_pretty};
+use serde_json::to_string_pretty;
 
 fn main() -> Result<(), ProgramError> {
     dotenv().ok();
@@ -36,7 +36,7 @@ fn main() -> Result<(), ProgramError> {
     File::create(output_file_path)?.write_all(to_string_pretty(&circuit.info)?.as_bytes())?;
 
     let report_file_path = build_output(&output_dir, "report", "json");
-    File::create(report_file_path)?.write_all(to_string(&report)?.as_bytes())?;
+    File::create(report_file_path)?.write_all(to_string_pretty(&report)?.as_bytes())?;
 
     Ok(())
 }
